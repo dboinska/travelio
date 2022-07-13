5e5091e4a1dd09a8aa6f90a4e524348add2c1b6a
Dorota Boinska <dboinska@gmail.com>
2022-07-14 01:20:28 +0200

    feat: add passport config, user model and register form

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+
+const userRoutes = require("./routes/users");
+const hotelRoutes = require("./routes/hotels");
+const reviewRoutes = require("./routes/reviews");
 
 const hotels = require("./routes/hotels");
 const reviews = require("./routes/reviews");
@@ -46,10 +53,26 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+});
+
+app.get("/userTest", async (req, res) => {
+  const user = new User({
+    email: "db@gmail.com",
+    username: "xxdbxx",
+  });
+  const newUser = await User.register(user, "password");
+  res.send(newUser);
 });
 
 const useReqTime = (req, res, next) => {
@@ -59,8 +82,9 @@ const useReqTime = (req, res, next) => {
 
 app.use(useReqTime);
 
-app.use("/hotels", hotels);
-app.use("/hotels/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/hotels", hotelRoutes);
+app.use("/hotels/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
