2487da217b3a2974cbf7e35c2a683ede1f015a4d
Dorota Boinska <dboinska@gmail.com>
2022-07-15 23:36:34 +0200

    fix: register route

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,11 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash("success", "Welcome to Travelio");
-      res.redirect("/hotels");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Travelio");
+        res.redirect("/hotels");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
