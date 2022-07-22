8062c682db90f2357c38ced736449af229ec9e2d
Dorota Boinska <dboinska@gmail.com>
2022-07-22 02:02:46 +0200

    feat: add a thumbnail virtual property

--- models/hotel.js
+++ models/hotel.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//  "https://res.cloudinary.com/dhnjbnqre/image/upload/w_300/v1658357341/Travelio/d765cvnbm20ebmjfwpbw.jpg";
+
 const Review = require("./review");
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 
 const HotelSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
