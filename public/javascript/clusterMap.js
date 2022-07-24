79ab7890d9589d758ad7756a3e5f092a834ee380
Dorota Boinska <dboinska@gmail.com>
2022-07-24 22:33:27 +0200

    feat:add styles to home page

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -7,17 +7,12 @@ const map = new mapboxgl.Map({
 });
 
 map.on("load", () => {
-  // Add a new source from our GeoJSON data and
-  // set the 'cluster' option to true. GL-JS will
-  // add the point_count property to your source data.
   map.addSource("hotels", {
     type: "geojson",
-    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
     data: hotels,
     cluster: true,
-    clusterMaxZoom: 14, // Max zoom to cluster points on
-    clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+    clusterMaxZoom: 14,
+    clusterRadius: 50,
   });
 
   map.addLayer({
@@ -26,11 +21,6 @@ map.on("load", () => {
     source: "hotels",
     filter: ["has", "point_count"],
     paint: {
-      // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-      // with three steps to implement three types of circles:
-      //   * Blue, 20px circles when point count is less than 100
-      //   * Yellow, 30px circles when point count is between 100 and 750
-      //   * Pink, 40px circles when point count is greater than or equal to 750
       "circle-color": [
         "step",
         ["get", "point_count"],
@@ -71,7 +61,6 @@ map.on("load", () => {
     },
   });
 
-  // inspect a cluster on click
   map.on("click", "clusters", (e) => {
     const features = map.queryRenderedFeatures(e.point, {
       layers: ["clusters"],
@@ -87,17 +76,10 @@ map.on("load", () => {
     });
   });
 
-  // When a click event occurs on a feature in
-  // the unclustered-point layer, open a popup at
-  // the location of the feature, with
-  // description HTML from its properties.
   map.on("click", "unclustered-point", (e) => {
     const { popUpMarkup } = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
 
-    // Ensure that if the map is zoomed out such that
-    // multiple copies of the feature are visible, the
-    // popup appears over the copy being pointed to.
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
@@ -112,9 +94,3 @@ map.on("load", () => {
     map.getCanvas().style.cursor = "";
   });
 });
-
-// {
-//     "type": "Feature",
-//     "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 },
-//     "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 0.0 ] }
-// },
