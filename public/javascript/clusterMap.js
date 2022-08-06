6f044f8a5ba49783323996c18fc696ce106d8052
Dorota Boinska <dboinska@gmail.com>
2022-08-06 10:31:02 +0200

    feat: adjust map scale

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -1,16 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "cluster-map",
-  style: "mapbox://styles/mapbox/dark-v10",
-  center: [-103.5917, 40.6699],
-  zoom: 3,
+  container: 'cluster-map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [-30, 40],
+  zoom: 2,
 });
 
-map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-map.on("load", () => {
-  map.addSource("hotels", {
-    type: "geojson",
+map.on('load', () => {
+  map.addSource('hotels', {
+    type: 'geojson',
     data: hotels,
     cluster: true,
     clusterMaxZoom: 14,
@@ -18,57 +18,57 @@ map.on("load", () => {
   });
 
   map.addLayer({
-    id: "clusters",
-    type: "circle",
-    source: "hotels",
-    filter: ["has", "point_count"],
+    id: 'clusters',
+    type: 'circle',
+    source: 'hotels',
+    filter: ['has', 'point_count'],
     paint: {
-      "circle-color": [
-        "step",
-        ["get", "point_count"],
-        "#03A9F4",
+      'circle-color': [
+        'step',
+        ['get', 'point_count'],
+        '#03A9F4',
         20,
-        "#9C27B0",
+        '#9C27B0',
         40,
-        "#76FF03",
+        '#76FF03',
         70,
-        "#F50057",
+        '#F50057',
       ],
-      "circle-radius": ["step", ["get", "point_count"], 18, 20, 24, 28, 38],
+      'circle-radius': ['step', ['get', 'point_count'], 18, 20, 24, 28, 38],
     },
   });
 
   map.addLayer({
-    id: "cluster-count",
-    type: "symbol",
-    source: "hotels",
-    filter: ["has", "point_count"],
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'hotels',
+    filter: ['has', 'point_count'],
     layout: {
-      "text-field": "{point_count_abbreviated}",
-      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-      "text-size": 12,
+      'text-field': '{point_count_abbreviated}',
+      'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+      'text-size': 12,
     },
   });
 
   map.addLayer({
-    id: "unclustered-point",
-    type: "circle",
-    source: "hotels",
-    filter: ["!", ["has", "point_count"]],
+    id: 'unclustered-point',
+    type: 'circle',
+    source: 'hotels',
+    filter: ['!', ['has', 'point_count']],
     paint: {
-      "circle-color": "#FFEA00",
-      "circle-radius": 6,
-      "circle-stroke-width": 1,
-      "circle-stroke-color": "#FFEA00",
+      'circle-color': '#FFEA00',
+      'circle-radius': 6,
+      'circle-stroke-width': 1,
+      'circle-stroke-color': '#FFEA00',
     },
   });
 
-  map.on("click", "clusters", (e) => {
+  map.on('click', 'clusters', e => {
     const features = map.queryRenderedFeatures(e.point, {
-      layers: ["clusters"],
+      layers: ['clusters'],
     });
     const clusterId = features[0].properties.cluster_id;
-    map.getSource("hotels").getClusterExpansionZoom(clusterId, (err, zoom) => {
+    map.getSource('hotels').getClusterExpansionZoom(clusterId, (err, zoom) => {
       if (err) return;
 
       map.easeTo({
@@ -78,7 +78,7 @@ map.on("load", () => {
     });
   });
 
-  map.on("click", "unclustered-point", (e) => {
+  map.on('click', 'unclustered-point', e => {
     const { popUpMarkup } = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
 
@@ -89,10 +89,10 @@ map.on("load", () => {
     new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
   });
 
-  map.on("mouseenter", "clusters", () => {
-    map.getCanvas().style.cursor = "pointer";
+  map.on('mouseenter', 'clusters', () => {
+    map.getCanvas().style.cursor = 'pointer';
   });
-  map.on("mouseleave", "clusters", () => {
-    map.getCanvas().style.cursor = "";
+  map.on('mouseleave', 'clusters', () => {
+    map.getCanvas().style.cursor = '';
   });
 });
